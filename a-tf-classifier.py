c716ac8c5911c398db051859353141db6e955c10
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2020-08-17 16:50:19 +0200

    - added -- class APathLabel to show the parent path of validation and training, and disable its widget if no path is set -- func _getParentDir to append training and valid. dir names to associated label frame's text label -- func _browseToDir to show the file dialogue via generic -- a size grip at root's bottom

--- a-tf-classifier.py
+++ a-tf-classifier.py
@@ -5,6 +5,11 @@
 #-------------------------------------------------------------------------------
 import tkinter as tk
 import tkinter.ttk as ttk
+
+from tkinter import filedialog
+# for getcwd
+import os
+
 def _geomTranslator(string, keywords):
     newString = []
     for i in string.split(';'):
@@ -38,9 +43,40 @@ class ScrollableCanvas(tk.Frame):
         _pack('self.sbH -> side = bottom ; fill = x', locals())
         _pack('self.c -> side = left ; expand = 1 ; fill = both', locals())
         self.c.bind('<Configure>',self.eventConfigure)
-        #print('a scrollable canvas created')
+        #print('a scrollable canvas created',self.c.bbox('all'))
     def eventConfigure(self,e):
         self.c['scrollregion'] = self.c.bbox('all')
+        #print('a scrollable canvas created2',self.c['scrollregion'])
+
+# disable the widget if no path is set
+class APathLabel(tk.Frame):
+    def __init__(self,master, **kwargs):
+        self.disabled = 1
+        self.disabledText = 'Please select File -> "{}"'.format(fileMenu.entrycget(0,'label'))
+        super().__init__(master, *kwargs)
+        self.l1 = ttk.Label(self, text = 'Parent Directory :', anchor = 'center')
+        self.l2 = ttk.Label(self, text = self.disabledText)
+        self.l2['anchor'] = 'center'
+        self._disable()
+        _pack('self.l1 -> side = left  ; padx = 10',locals())
+        _pack('self.l2 -> side = right ; expand = 1 ; fill = x',locals())
+
+    # a pass-through config, to only remove a -text option
+    def config(self, **kwargs):
+        text = kwargs.pop('text',False)
+        self._enable()
+        call, text = [(self._disable,self.disabledText),(self._enable,text)][bool(text)]
+        print('+',call,text)
+        call()
+        self.l2['text'] = text
+
+    def _enable(self):
+        self.disabled = 0
+        self.l2['state'] = 'normal'
+    def _disable(self):
+        self.disabled = 1
+        self.l2['state'] = 'disabled'
+
 def _center(w):
     w.geometry('+{}+{}'.format(int(w.winfo_screenwidth()/2 - w.winfo_reqwidth()/2),int(w.winfo_screenheight()/2 - w.winfo_reqheight()/2)))
 
@@ -60,6 +96,25 @@ def _makeABanner(p, title = 'Banner'):
     _pack('l -> fill = x', locals())
     _pack('s -> expand = 1 ; fill = x', locals())
     _pack('f -> side = top ; expand = 1 ; fill = x',locals())
+
+# base func to select directory dialog
+def _browseToDir(_dir,_title):
+    path = filedialog.askdirectory(initialdir = _dir, title = _title)
+    return path
+
+# File -> (index 0) ; of validation and training dirs
+def _getParentDir():
+    path = _browseToDir(os.getcwd(), fileMenu.entrycget(0,'label'))
+    pathFrame.config(text = '"{}"'.format(path))
+    if not pathFrame.disabled:
+        _all = os.listdir(path)
+        a = _all[0]
+        b = _all[1]
+        # remove everything after "dataset"
+        oneFrame['text'] = " ".join (str.split(oneFrame['text'])[:3] + ['"{}"'.format(a)])
+        twoFrame['text'] = " ".join (str.split(oneFrame['text'])[:3] + ['"{}"'.format(b)])
+
+
 # root window
 main = tk.Tk()
 
@@ -73,6 +128,7 @@ _alterToplevelClose(aboutWindow)
 # menus
 rootMenu = tk.Menu(main)
 fileMenu = tk.Menu(rootMenu,tearoff = 0)
+fileMenu.add_command(label = 'Select the Directory containing Training and Validiation dirs', command = _getParentDir)
 fileMenu.add_separator()
 fileMenu.add_command(label = 'Exit', command = lambda : _exitMain(main))
 helpMenu = tk.Menu(rootMenu, tearoff = 0)
@@ -82,18 +138,33 @@ rootMenu.add_cascade(menu = fileMenu, label = 'File')
 rootMenu.add_cascade(menu = helpMenu, label = 'Help')
 main['menu']=rootMenu
 
+# size grip
+grip = ttk.Sizegrip(main)
+_pack('grip -> side = bottom ; fill = x')
+
+
 # root window 'body' frame
 zeroFrame = ttk.LabelFrame(main, text = 'Zero')
 # pack it to fully occupy the root windo
 _pack('zeroFrame -> expand = 1 ; fill = both')
 
-# train data
+# show parent path
+pathFrame = APathLabel(zeroFrame)
+_pack('pathFrame -> side = top ; fill = x')
+
+# train data gallery
 oneFrame = ttk.LabelFrame(zeroFrame,text = 'Training Images dataset')
 oneScrollable = ScrollableCanvas(oneFrame, pack = 'expand = 1 ; fill = both')
 _pack('oneFrame -> expand = 1 ; fill = both')
 
+# validation
+twoFrame = ttk.LabelFrame(zeroFrame,text = 'Validation Images dataset')
+twoScrollable = ScrollableCanvas(twoFrame, pack = 'expand = 1 ; fill = both')
+_pack('twoFrame -> expand = 1 ; fill = both')
 
 #_makeABanner(oneFrame, 'Training Data')
 main.title('A TF Classifier')
+
+
 _center(main)
 main.mainloop()
