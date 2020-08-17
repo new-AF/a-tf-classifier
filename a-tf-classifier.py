38e5d31a0d51023123a085834f4649e7fcccccec
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2020-08-17 12:47:53 +0200

    - added -- menus -- a new toplevel window, and support functions

--- a-tf-classifier.py
+++ a-tf-classifier.py
@@ -6,11 +6,27 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
-def center(w):
-    if type(w)==tk.Tk:
-        w.geometry('+{}+{}'.format(int(w.winfo_screenwidth()/2 - w.winfo_reqwidth()/2),int(w.winfo_screenheight()/2 - w.winfo_reqheight()/2)))
+def _center(w):
+    w.geometry('+{}+{}'.format(int(w.winfo_screenwidth()/2 - w.winfo_reqwidth()/2),int(w.winfo_screenheight()/2 - w.winfo_reqheight()/2)))
 
+def _showToplevel(w):
+    w.deiconify()
+    _center(w)
+def _hideToplevel(w):
+    w.withdraw()
+def _alterToplevelClose(w):
+    w.wm_protocol("WM_DELETE_WINDOW", func = lambda i=w: _hideToplevel(i) )
 main = tk.Tk()
-main.title('A TF Frontend')
-center(main)
+aboutWindow = tk.Toplevel(main)
+aboutWindow.title('About A TF Classifier')
+_hideToplevel(aboutWindow)
+_alterToplevelClose(aboutWindow)
+rootMenu = tk.Menu(main)
+fileMenu = tk.Menu(rootMenu,tearoff = 0)
+helpMenu = tk.Menu(rootMenu, tearoff = 0)
+helpMenu.add_command(label='About', command = lambda : _showToplevel(aboutWindow))
+rootMenu.add_cascade(menu = helpMenu, label = 'Help')
+main['menu']=rootMenu
+main.title('A TF Classifier')
+_center(main)
 main.mainloop()
