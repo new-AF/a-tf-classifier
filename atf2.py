588734e563ee0152be98ba7bd549178b7131de0f
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2020-10-18 12:45:48 +0200

    added class MyGrid.

--- atf2.py
+++ atf2.py
@@ -23,6 +23,46 @@ class MyTk(tk.Tk):
         if center:
             self.center()
 
-root = MyTk(400,100)
-
+class MyGrid:
+    def my_grid(self,**kw):
+        parent = self.master
+        for i,t in kw.items():
+            if i in ('row','column'):
+                if (len_t:=len(t))==0:
+                    continue
+                # row-1,col-1
+                t=(t[0]-1,*t[1:])
+                if len_t == 4:
+                    # row , row_span , uniform_id , weight
+                    kw2 = {i: t[0] , f'{i}span':t[1]}
+                    self.grid(**kw2) # CORE
+                    method = getattr(tk.Grid,f'grid_{i}configure')
+                    method(parent , t[0] , uniform = t[2] , weight = t[3]) # CORE
+                elif len_t == 3:
+                    # row , row_span , uniform_id
+                    kw2 = {i: t[0] , f'{i}span':t[1]}
+                    self.grid(**kw2) # CORE
+                    method = getattr(tk.Grid,f'grid_{i}configure')
+                    method(parent , t[0] , uniform = t[2]) # CORE
+                elif len_t == 2:
+                    # row , row_span
+                    kw2 = {i: t[0] , f'{i}span':t[1]}
+                    self.grid(**kw2) # CORE
+                elif len_t == 1:
+                    # row
+                    kw2 = {i: t[0]}
+                    self.grid(**kw2) # CORE
+            elif i == 'sti':
+                # sticky
+                self.grid_configure(sticky = t)
+    
+class MyButton(tk.Button , MyGrid):
+    def __init__(self,parent,**kw):
+        super().__init__(parent,**kw)
+           
+root = MyTk(400,300)
+b = MyButton(root,text='b1')
+bb = MyButton(root,text='b2')
+b.my_grid(row=(1,1,),column=(1,1,0,1),sti='nswe')
+bb.my_grid(row=(1,1),column=(2,1,0,12),sti='nswe')
 root.mainloop()
